9cae1ab3c0e23f00ee4eff11a4507e54b4362b97
Bakr <177347891+bkrzq@users.noreply.github.com>
2026-06-17 08:29:16 +0300

    AddTask and RemoveTask functions

--- src/data.js
+++ src/data.js
@@ -7,14 +7,37 @@ const user = {
 
 
 
-const task = {
-  id: 101,
-  title: "buy groceries",
-  done: false,
-  userId: 1
+const tasks = [
+  {
+    id: 101,
+    title: "buy groceries",
+    done: false,
+    userId: 1
+   }
+  ]
+
+
+const addTask = (title, userId) => {
+  const newTask = {
+    id: Date.now(),
+    title: title,
+    done: false,
+    userId: userId
+    }  
+  tasks.push(newTask)
+  return newTask
+ 
  }
 
 
+const removeTask = (id) => {
+    const index = tasks.findIndex((task) => task.id === id)
+    tasks.splice(index, 1)
+}
 
-console.log(user)
-console.log(task)
+
+
+addTask("study javascript", 1)
+console.log("Before:", tasks)
+removeTask(101)
+console.log("After:", tasks)
